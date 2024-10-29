7757339d6a761b4a8bcaefe75707606347cff21a
AndresLopezdelRio <alopser1801@g.educaand.es>
2024-10-29 22:25:11 +0100

    Añadido el proyecto Java para sumar dos números

---
+++ SumaDosNumeros/src/main/main.java
@@ -0,0 +1,10 @@
+package main;
+
+public class main {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int suma = num1 + num2;
+        System.out.println("La suma es: " + suma);
+    }
+}
